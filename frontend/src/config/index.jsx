9de452f3b25d71b83bf39dbea6e38a52f604c722
Shazia Anwar <shazi.522122595@gmail.com>
2025-07-02 21:28:33 +0530

    Frontend Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = "https://linkedin-clone-sg1l.onrender.com"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
